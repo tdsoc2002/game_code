78305d87f04a82831f2ece9bc926176dcf19cfe4
ErJiQwQ <2776483968@qq.com>
2022-08-01 09:47:36 +0800

    修改题目 错误原因：思维混乱，不考虑数据问题

--- src/luogu/P5735.java
+++ src/luogu/P5735.java
@@ -3,26 +3,17 @@ package luogu;
 import java.util.Scanner;
 
 public class P5735 {
+}
+class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double result=0;
-        double a1= scanner.nextDouble();
-        double a2= scanner.nextDouble();
-        double b1= scanner.nextDouble();
-        double b2= scanner.nextDouble();
-        double c1= scanner.nextDouble();
-        double c2= scanner.nextDouble();
-        result+=run(a1,a2,b1,b2);
-        result+=run(c1,c2,b1,b2);
-        result+=run(c1,c2,a1,a2);
-        System.out.println(result);
-    }
-    public static double run(double x1,double y1,double x2,double y2){
-        double result=0;
-        double num1 =  Math.pow(x2-x1,2);
-        double num2 =  Math.pow(y2-y1,2);
-        result= Math.sqrt(num1-num2);
-        System.out.println(result);
-        return result;
+        double x1 = scanner.nextDouble(), y1 = scanner.nextDouble(), x2 = scanner.nextDouble(),
+                y2 = scanner.nextDouble(), x3 = scanner.nextDouble(), y3 = scanner.nextDouble();
+        double l1 = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+        double l2 = Math.sqrt(Math.pow(x3-x2, 2) + Math.pow(y3-y2, 2));
+        double l3 = Math.sqrt(Math.pow(x3-x1, 2) + Math.pow(y3-y1, 2));
+        System.out.printf("%.2f", l1+l2+l3);
+        scanner.close();
     }
 }
+
